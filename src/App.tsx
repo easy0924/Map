a8b9d2242096dd4a7e2d90171f223f497a615e68
Ge Wu <gew@microsoft.com>
2021-06-02 00:52:47 -0700

    live reload

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Map
         </p>
         <a
           className="App-link"
